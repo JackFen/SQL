3e253c8e7a6bf10e25c691d94e9f2caa5d633815
JackFen <96562354+JackFen@users.noreply.github.com>
2022-02-21 18:10:03 +0800

    打卡学习第10天
    今天把约束的后半部分学完了

--- 约束笔记.sql
+++ 约束笔记.sql
@@ -270,3 +270,218 @@ add primary key(id);
 #4.3 如何删除主键约束(在实际开发中根本不会去删除主键约束！)
 alter table test6
 drop primary key;
+
+#6.1自增长列： AUTO_INCREMENT
+#6.1在create table时添加
+create table test7(
+id int primary key auto_increment,
+last_name varchar(25)
+);
+#开发中，一旦主键作用的字段上声明有auto_increment,则我们在添加数据时就不会给主键对应的字段去赋值了
+insert into test7(last_name)
+values('Tom');
+
+select * from test7;
+
+insert into test7(last_name)
+values('Tom');
+
+#当我们向主键（含auto_increment）的字段上添加0或者null时，实际上会自动的往上添加指定的字段的数值
+insert into test7(id,last_name)
+values(0,'Tom');
+
+insert into test7(id,last_name)
+values(null,'Tom');
+
+insert into test7(id,last_name)
+values(10,'Tom');
+
+insert into test7(id,last_name)
+values(-10,'Tom');
+
+#6.1在alter table时添加
+create table test8(
+id int primary key,
+last_name varchar(25)
+);
+
+alter table test8
+modify id int auto_increment;
+
+#6.3在alter table时删除
+
+alter table test8
+modify id int;
+
+#6.4MySQL8.0新特性-自增变量的持久化
+
+#8.check约束
+#MySQL5.7不支持check约束，MySQL8.0支持check约束
+create table test10(
+id int,
+last_name varchar(10),
+salary decimal(10,2) check(salary>2000)
+);
+
+insert into test10
+values(1,'Tom',2500);
+#添加失败
+insert into test10
+values(1,'Tom',1500);
+
+#9.default约束
+create table test11(
+id int,
+last_name varchar(15),
+salary decimal(10,2) default 2000
+);
+
+desc test11;
+
+insert into test11(id,last_name,salary)
+values(1,'Tom',3000);
+
+insert into test11(id,last_name)
+values(1,'Tom1');
+
+select * from test11;
+
+#9.2 在alter table时添加约束
+create table test12(
+id int,
+last_name varchar(15),
+salary decimal(10,2)
+);
+
+desc test12;
+
+alter table test12
+modify salary decimal(8,2) default 2500;
+
+#9.3 在alter table时删除约束
+
+alter table test12
+modify salary decimal(8,2);
+
+#课后练习
+
+#练习1：
+create database test04_emp;
+use test04_emp;
+create table emp2(
+id int,
+emp_name varchar(15)
+);
+
+create table dept2(
+id int,
+dept_name varchar(15)
+);
+
+#1.向表emp2的id列中添加primary key约束
+alter table emp2
+add constraint pk_emp2_id primary key(id);
+
+#2.向表dept2的id列中添加primary key约束
+alter table dept2
+add primary key(id);
+
+#向表emp2中添加dept_id，并在其中定义foreign key约束，与之相关的列是dept2表中的id列
+
+alter table emp2
+add dept_id int;
+
+desc emp2;
+
+alter table emp2
+add constraint fk_emp2_deptid foreign key(dept_id) references dept2(id);
+
+#练习2：
+
+use test01_library;
+
+desc books;
+
+#根据题目要求给books表中的字段添加约束
+#方式1：
+alter table books
+add primary key(id);
+
+alter table books
+modify id int auto_increment;
+#方式2：
+alter table books
+modify id int primary key auto_increment;
+
+#针对于非di字段的操作
+alter table books
+modify name varchar(50) not null;
+
+alter table books
+modify authors varchar(100) not null;
+
+alter table books
+modify price float not null;
+
+alter table books
+modify pubdate year not null;
+
+alter table books
+modify num int not null;
+
+#练习3：
+#1.创建数据库test04_company
+create database if not exists test04_company character set 'utf8';
+
+use test04_company;
+#2.按照下表给出的表结构在test04_company数据库中创建两个数据表offices和employees
+create table if not exists offices(
+officeCode int(10) primary key,
+city varchar(50) not null,
+address varchar(50),
+country varchar(50)not null,
+postalCode varchar(15),
+constraint uk_off_poscode unique(postalCode)
+);
+
+desc offices;
+create table employees(
+employeeNumber int primary key auto_increment,
+lastName varchar(50) not null,
+firstName varchar(25) unique,
+mobile varchar(25) unique,
+officeCode int(10) not null,
+jobTitle varchar(50) not null,
+birth datetime not null,
+note varchar(255),
+sex varchar(5),
+constraint fk_emp_offcode foreign key(officeCode) references offices(officeCode)
+);
+
+desc employees;
+
+#3.将表employees的mobile字段修改到officeCode 字段后面
+alter table employees
+modify mobile varchar(25) after officeCode;
+#4.将表employees的birth字段改名为employee_birth
+alter table employees
+change birth employee_birth datetime;
+#5.修改sex字段，数据类型为char(1)，非空约束
+alter table employees
+modify sex CHAR(1) not null;
+
+#6.删除字段note
+alter table employees
+drop column note;
+
+#7.增加字段名favoriate_activity,数据类型为varchar(100)
+alter table employees
+add favoriate_activity varchar(100);
+
+#8.将表employees名称修改为employees_info
+rename table employees
+to employees_info;
+#错误：Table 'test04_company.employees' doesn't exist
+desc employees;
+#正确
+desc employees_info;
